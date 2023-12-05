bfa654aaa9915e95fe527e54711c87436e1bb7d9
Sameer Acharya <acharyasameer2002@gmail.com>
2023-12-04 21:54:15 -0500

    Implement main function for input handling.

--- mips-fibonacci.asm
+++ mips-fibonacci.asm
@@ -3,3 +3,12 @@
     error_message: .asciiz "Illegal Number!\n"
     line_break: .asciiz "\n"
 
+.text
+main:
+    li $v0, 4
+    la $a0, prompt_message
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t1, $v0
