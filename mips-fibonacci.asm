7b3ecb3ce5f53bdecbbac7c3d43e9be3448a6a71
Sameer Acharya <acharyasameer2002@gmail.com>
2023-12-04 22:22:13 -0500

    Implement input loop to prompt user for a valid integer.

--- mips-fibonacci.asm
+++ mips-fibonacci.asm
@@ -2,3 +2,23 @@
 prompt:             .asciiz "Enter an integer (N >= 25): "
 error_display:      .asciiz "Illegal Number! Please Write valid numbers i.e (N >= 25) \n"
 fib_display:        .asciiz "Fibonacci Sequence:\n"
+
+.text
+main:
+    j input_loop
+
+input_loop:
+    li $v0, 4
+    la $a0, prompt
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t0, $v0
+
+    bge $t0, 25, fibonacci_sequence
+
+    li $v0, 4
+    la $a0, error_display
+    syscall
+    j input_loop
