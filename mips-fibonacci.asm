5a62eb4be98294759dbde1c287189fe5db93ea4a
Sameer Acharya <acharyasameer2002@gmail.com>
2023-12-04 22:19:11 -0500

    fixing bugs and restarting

--- mips-fibonacci.asm
+++ mips-fibonacci.asm
@@ -1,46 +1,54 @@
 .data
-    prompt_message: .asciiz "Enter an integer: "
-    error_message: .asciiz "Illegal Number!\n"
-    line_break: .asciiz "\n"
-
+prompt: .asciiz "Enter an integer (N >= 25): "
+error_display: .asciiz "Illegal Number! Please write valid numbers, i.e., (N >= 25) \n"
+fib_display: .asciiz "Fibonacci Sequence:\n"
 .text
 main:
+    j input_loop
+input_loop:
     li $v0, 4
-    la $a0, prompt_message
+    la $a0, prompt
     syscall
 
     li $v0, 5
     syscall
-    move $t1, $v0
+    move $t0, $v0
 
-loop1:
-    bgt $t1, 24, find_fibonacci
+    bge $t0, 25, fibonacci_sequence
     li $v0, 4
-    la $a0, error_message
+    la $a0, error_display
     syscall
+    j input_loop
+    
+fibonacci_sequence:
+    li $t1, 0
+    li $t2, 1
+    li $t3, 0
 
     li $v0, 4
-    la $a0, prompt_message
+    la $a0, fib_display
     syscall
 
-    li $v0, 5
-    syscall
-    move $t1, $v0
-    j loop1
-
-find_fibonacci:
-    move $a1, $v0
-    li $s0, 0
-    li $s1, 1
-    ...
-    li $v0, 4
-    la $a0, line_break
+fibonacci_loop:
+    li $v0, 1
+    move $a0, $t1
     syscall
 
-loop2:
-    ...
-    j loop2
+    add $t4, $t1, $t2
+    move $t1, $t2
+    move $t2, $t4
+    addi $t3, $t3, 1
 
-end_program:
+    li $v0, 11
+    li $a0, 32
+    syscall
+    bge $t3, $t0, exit_code
+
+    j fibonacci_loop
+exit_code:
+    li $v0, 1
+    move $a0, $t1
+    syscall
+
     li $v0, 10
     syscall
