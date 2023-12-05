6c780d38d5042c8677b44ae469c87bbc7ee1ab50
Sameer Acharya <acharyasameer2002@gmail.com>
2023-12-04 21:55:36 -0500

    Add loop for input validation and re-prompt.

--- mips-fibonacci.asm
+++ mips-fibonacci.asm
@@ -12,3 +12,18 @@ main:
     li $v0, 5
     syscall
     move $t1, $v0
+
+loop1:
+    bgt $t1, 24, find_fibonacci
+    li $v0, 4
+    la $a0, error_message
+    syscall
+
+    li $v0, 4
+    la $a0, prompt_message
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t1, $v0
+    j loop1
